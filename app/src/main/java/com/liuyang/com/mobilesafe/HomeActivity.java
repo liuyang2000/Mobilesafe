ee08e8b5925e66c65c80622c3008c27eef56b8c0
liuyang2000 <847479481@qq.com>
2016-08-06 15:39:57 +0800

    功能列表九宫格菜单

--- app/src/main/java/com/liuyang/com/mobilesafe/HomeActivity.java
+++ app/src/main/java/com/liuyang/com/mobilesafe/HomeActivity.java
@@ -2,15 +2,83 @@ package com.liuyang.com.mobilesafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 /**
  * 主界面
  */
 public class HomeActivity extends AppCompatActivity {
 
+    private GridView gv_home;
+    private String[] mTitleStrs;
+    private int[] mDrawableIds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initUI();
+
+        // 初始化数据
+        initData();
+    }
+
+    private void initData() {
+        //准备数据(文字(9组),图片(9张))
+        mTitleStrs = new String[]{
+                "手机防盗","通信卫士","软件管理","进程管理","流量统计","手机杀毒","缓存清理","高级工具","设置中心"
+        };
+
+        mDrawableIds = new int[]{
+                R.drawable.home_safe,R.drawable.home_callmsgsafe,
+                R.drawable.home_apps,R.drawable.home_taskmanager,
+                R.drawable.home_netmanager,R.drawable.home_trojan,
+                R.drawable.home_sysoptimize,R.drawable.home_tools,R.drawable.home_settings
+        };
+
+        // 九宫格控件设置数据适配器
+        gv_home.setAdapter(new MyAdapter());
+
+    }
+
+    private void initUI() {
+        gv_home = (GridView) findViewById(R.id.gv_home);
+
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mTitleStrs.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mTitleStrs[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = View.inflate(getApplicationContext(), R.layout.gridview_item, null);
+            TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
+            ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+
+            tv_title.setText(mTitleStrs[position]);
+            iv_icon.setBackgroundResource(mDrawableIds[position]);
+            return view;
+        }
     }
 }
