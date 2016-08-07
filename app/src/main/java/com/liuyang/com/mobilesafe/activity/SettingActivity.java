c3b775322eedaab9028cb3581cfa9f00e40c62b9
liuyang2000 <847479481@qq.com>
2016-08-07 22:30:13 +0800

    day02_9,选中SettingItemView条目状态切换(有点问题，选择checkbox ,无法实现条目状态更新)

--- app/src/main/java/com/liuyang/com/mobilesafe/activity/SettingActivity.java
+++ app/src/main/java/com/liuyang/com/mobilesafe/activity/SettingActivity.java
@@ -2,8 +2,10 @@ package com.liuyang.com.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.liuyang.com.mobilesafe.R;
+import com.liuyang.com.mobilesafe.view.SettingItemView;
 
 /**
  * Created by liuyang on 2016/8/6.
@@ -14,5 +16,21 @@ public class SettingActivity extends Activity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_setting);
+
+        initUpdate();
+    }
+
+    private void initUpdate() {
+        final SettingItemView siv_update = (SettingItemView) findViewById(R.id.siv_update);
+
+        siv_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获取之前的选中状态
+                boolean isCheck = siv_update.isCheck();
+                // 设置相反的状态
+                siv_update.setCheck(!isCheck);
+            }
+        });
     }
 }
