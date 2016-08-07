b0a8a32638e41664d1adbe9bee4d034869118f77
liuyang2000 <847479481@qq.com>
2016-08-07 23:08:09 +0800

    day02_12,sp工具类编写

---
+++ app/src/main/java/com/liuyang/com/mobilesafe/util/SpUtils.java
@@ -0,0 +1,41 @@
+package com.liuyang.com.mobilesafe.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by liuyang on 2016/8/7.
+ */
+public class SpUtils {
+
+    private static SharedPreferences sp;
+
+    /**
+     * 保存Boolean类型的值
+     * @param ctx 上下文环境
+     * @param key
+     * @param value
+     */
+    public static void putBoolean(Context ctx, String key, boolean value) {
+        if (sp == null) {
+            sp = ctx.getSharedPreferences("config", ctx.MODE_PRIVATE);
+        }
+
+        sp.edit().putBoolean(key, value).commit();
+    }
+
+    /**
+     * 获取boolean 类型的值
+     * @param ctx 上下文环境
+     * @param key
+     * @param defValue 默认值， 如果获取不到 key 对应的value ,则返回 defValue
+     * @return
+     */
+    public static boolean getBoolean(Context ctx, String key, boolean defValue){
+        if(sp == null){
+            sp = ctx.getSharedPreferences("config", ctx.MODE_PRIVATE);
+        }
+
+        return sp.getBoolean(key, defValue);
+    }
+}
