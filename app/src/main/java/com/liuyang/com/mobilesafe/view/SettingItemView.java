2598faf9c2d8cbb807fa446d4228b1117056422f
liuyang2000 <847479481@qq.com>
2016-08-15 22:55:32 +0800

    day02_17,构造方法中获取自定义属性值 18,给自定义组合控件内部控件赋值

--- app/src/main/java/com/liuyang/com/mobilesafe/view/SettingItemView.java
+++ app/src/main/java/com/liuyang/com/mobilesafe/view/SettingItemView.java
@@ -2,6 +2,7 @@ package com.liuyang.com.mobilesafe.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RelativeLayout;
@@ -13,9 +14,16 @@ import com.liuyang.com.mobilesafe.R;
  * Created by liuyang on 2016/8/6.
  */
 public class SettingItemView extends RelativeLayout {
+    private static final String NAMESPACE = "http://schemas.android.com/apk/res/com.liuyang.com.mobilesafe";
     TextView tv_title;
     TextView tv_desc;
     CheckBox cb_box;
+
+    String mDesctile;
+    String mDescoff;
+    String mDescon;
+    private static final String tag = "SettingItemView";
+
     /**
      * 重写了构造方法，调用两个参数的方法
      * @param context
@@ -47,6 +55,22 @@ public class SettingItemView extends RelativeLayout {
         tv_title = (TextView) findViewById(R.id.tv_title);
         tv_desc = (TextView) findViewById(R.id.tv_desc);
         cb_box = (CheckBox) findViewById(R.id.cb_box);
+
+        //获取自定义属性或原生属性的操作，从atts中获取
+        initAttrs(attrs);
+
+        //为ITem的title赋值
+        tv_title.setText(mDesctile);
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        Log.i(tag,"attrs.getAttributeCount() : " + attrs.getAttributeCount());
+
+        mDesctile = attrs.getAttributeValue(NAMESPACE, "desctitle");
+        mDescoff = attrs.getAttributeValue(NAMESPACE, "descoff");
+        mDescon = attrs.getAttributeValue(NAMESPACE, "descon");
+
+
     }
 
     /**
@@ -64,9 +88,9 @@ public class SettingItemView extends RelativeLayout {
     public void setCheck(boolean isCheck){
         cb_box.setChecked(isCheck);
         if(isCheck){
-            tv_desc.setText("自动更新已开启");
+            tv_desc.setText(mDescon);
         } else{
-            tv_desc.setText("自动更新已关闭");
+            tv_desc.setText(mDescoff);
         }
     }
 }
